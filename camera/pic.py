7b77ad965da38178ab8da709bd1857357f3fee23
Avi <avipars@users.noreply.github.com>
2024-11-02 18:58:27 +0200

    Update pic.py

--- camera/pic.py
+++ camera/pic.py
@@ -3,7 +3,7 @@ from picamzero import Camera
 cam = Camera()
 print("cam opened")
 cam.resolution = (1024, 768) 
-#cam.start_preview()
+#cam.start_preview() # commented out because in headless mode there will be no way to deal with a preview
 cam.take_photo("image.jpg")
-#cam.stop_preview()
+#cam.stop_preview() # see above
 print("cam done")
