9a3826c7587768e4be1b90c9836f87c5d556825f
Avi <avipars@users.noreply.github.com>
2024-11-02 19:01:24 +0200

    Delete camera/vid.sh

--- camera/vid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export DISPLAY=:0
-libcamera-vid -t 0 -f --width 1920 --height 1080 --framerate 24 --exposure normal --awb auto --brightness -0. --vflip --hflip #take a video in 1080p with 24 frames, auto white balance, flip vertically and horizontally 
